2eb422a6a2d074e06fe3cb82769d8d01cbde23bb
NoFox <nfox598625@gmail.com>
2024-04-02 14:24:15 -0400

    added more of the models part to get setup for graphql. Also setup type queries each models and tested each one to see if they work correctly.

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,6 @@
 const User = require("./User");
+const Order = require("./Order");
+const Category = require("./Category");
+const Product = require("./Product");
 
-module.exports = { User };
+module.exports = { User, Order, Category, Product };
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const { GraphQLError } = require("graphql");
 const jwt = require("jsonwebtoken");
 
-const secret = "mysecretssshhhhhhh";
+const secret = "secret";
 const expiration = "2h";
 
 module.exports = {
