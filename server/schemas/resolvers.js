696547928c667ac602f7a60fbdc05fdd1103fea8
NoFox <nfox598625@gmail.com>
2024-04-23 16:55:22 -0400

    added resovlers for the Query typedefs now need to complete the mutations

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,91 @@
+const { User, Product, Theme, Order } = require("../models");
+const auth = require("../utils/auth");
+const { signToken, AuthenticationError } = require("../utils/auth");
+const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
+
+const resolvers = {
+  Query: {
+    theme: async () => {
+      return await Theme.find();
+    },
+    products: async (parent, { theme, name }) => {
+      const params = {};
+
+      if (theme) {
+        params.theme = theme;
+      }
+
+      if (name) {
+        params.name = {
+          $regex: name,
+        };
+      }
+
+      return await Product.findById(params).populate("theme");
+    },
+    product: async (parent, { _id }) => {
+      return await Product.findById(_id).populate("theme");
+    },
+    user: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).populate({
+          path: "orders.products",
+          populate: "theme",
+        });
+
+        user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+
+        return user;
+      }
+
+      throw AuthenticationError;
+    },
+    order: async (parent, { _id }, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).populate({
+          path: "order.products",
+          populate: "theme",
+        });
+        return user.orders.id(_id);
+      }
+
+      throw AuthenticationError;
+    },
+    checkout: async (parent, args, context) => {
+      const url = new URL(context.headers.referer).origin;
+      const order = new Order({ products: args.products });
+      const line_items = [];
+
+      const { products } = await order.populate("products");
+
+      for (let i = 0; i < products.length; i++) {
+        const product = await stripe.products.create({
+          name: products[i].name,
+          description: products[i].description,
+          images: [`${usrl}/images/${products[i].image}`],
+        });
+
+        const price = await stripe.prices.create({
+          product: product.id,
+          unit_amount: products[i].price * 100,
+          currency: "usd",
+        });
+
+        line_items.push({
+          price: price.id,
+          quantity: 1,
+        });
+      }
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items,
+        mode: "payment",
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}/`,
+      });
+
+      return { session: session.id };
+    },
+  },
+};
