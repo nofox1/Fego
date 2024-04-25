3e8bc564a901c5af0d6f7aaa97c01c29ab69cb9b
NoFox <nfox598625@gmail.com>
2024-04-25 19:19:25 -0400

    updated main.jsx and then created files in the new folder pages

--- client/Fego/src/main.jsx
+++ client/Fego/src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter} from 'react-router-dom'
 import './index.css'
 
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
+import Error from './pages/Error.jsx'
+import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    error: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home/>
+      }, {
+        path: '/Login',
+        element: <Login/>
+      }, {
+        path: '/signup',
+        element: <Signup/>
+      }, 
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <RouterProvider router={router} />
 )
